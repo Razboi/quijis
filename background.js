794e17bcbac4e6e174f89b0250c400416843f531
Marc <recatala.marc@gmail.com>
2021-01-28 21:15:17 +0100

    Switched from webRequest to debugger API for network request recordings

--- background.js
+++ background.js
@@ -1,27 +1,51 @@
-chrome.declarativeContent.onPageChanged.removeRules(undefined, function () {
+chrome.declarativeContent.onPageChanged.removeRules(undefined, () => {
   chrome.declarativeContent.onPageChanged.addRules([{
     conditions: [new chrome.declarativeContent.PageStateMatcher()],
     actions: [new chrome.declarativeContent.ShowPageAction()]
   }]);
 });
 
-const handleNetworkError = (error) => {
-  chrome.storage.sync.get(["networkErrors"], function ({ networkErrors }) {
-    networkErrors.push(JSON.stringify(error));
-    chrome.storage.sync.set({ networkErrors: networkErrors });
-  });
+const handleNetworkEvent = (currentTabId, debuggeeId, message, params) => {
+  // chrome.storage.sync.get(["networkErrors"], ({ networkErrors }) => {
+  //   networkErrors.push(JSON.stringify(error));
+  //   chrome.storage.sync.set({ networkErrors: networkErrors });
+  // });
+  // TODO: Filter errors and store them
+  if (message === "Network.responseReceived" && params.type === "XHR") {
+    chrome.debugger.sendCommand({ tabId: currentTabId }, "Network.getResponseBody", { "requestId": params.requestId }, (response) => {
+      if (response) {
+        console.log(response.base64Encoded ? console.log(response.body) : JSON.parse(response.body));
+      }
+    });
+  }
 }
 
 const startRecordingNetworkEvents = () => {
   chrome.storage.sync.set({ networkErrors: [] });
-  chrome.webRequest.onErrorOccurred.addListener(handleNetworkError, { urls: ["<all_urls>"] });
+
+  chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+    const currentTabId = tabs[0].id;
+
+    chrome.debugger.attach({ tabId: currentTabId }, "1.0", () => {
+      // TODO: Handle error attaching debugger
+      if (chrome.runtime.lastError) {
+        alert(chrome.runtime.lastError.message);
+        return;
+      }
+      chrome.debugger.sendCommand({ tabId: currentTabId }, "Network.enable");
+      chrome.debugger.onEvent.addListener((...params) => handleNetworkEvent(currentTabId, ...params));
+    });
+  })
 }
 
 const stopRecordingNetworkEvents = () => {
-  chrome.webRequest.onErrorOccurred.removeListener(handleNetworkError);
+  chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+    const currentTabId = tabs[0].id;
+    chrome.debugger.detach({ tabId: currentTabId });
+  });
 }
 
-chrome.runtime.onMessage.addListener(function (message) {
+chrome.runtime.onMessage.addListener((message) => {
   if (message == "startRecordingNetworkEvents") {
     startRecordingNetworkEvents();
   } else if (message == "stopRecordingNetworkEvents") {
